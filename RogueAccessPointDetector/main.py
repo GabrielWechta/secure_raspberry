2caaa7b6d4080446c882f2caa141436db9378176
Gabriel Wechta <wechtagabriel@gmail.com>
2021-10-23 18:40:17 +0200

    SYN/ACK apture and datframe done

--- RogueAccessPointDetector/main.py
+++ RogueAccessPointDetector/main.py
@@ -1,7 +1,20 @@
 from time import sleep
 
+import network_manager_utils
 from TCP_IP_utils import start_3_way_handshake
 
 if __name__ == "__main__":
-    for i in range(10):
+    for i in range(100):
         start_3_way_handshake('172.217.16.3', 80)
+
+    sleep(2)
+
+    network_manager_utils.connect_to_network_by_bssid("6C:60:EB:87:88:99")
+
+    sleep(4)
+
+    for i in range(100):
+        start_3_way_handshake('172.217.16.3', 80)
+
+    network_manager_utils.connect_to_network_by_bssid("64:66:B3:1E:26:EF")
+
